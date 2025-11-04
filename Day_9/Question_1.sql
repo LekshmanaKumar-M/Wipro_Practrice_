a1fdba427e440bd9ea6ea93a1dae6d677d05dcb2
LekshmanaKumar-M <159934758+LekshmanaKumar-M@users.noreply.github.com>
2025-11-04 16:40:45 +0530

    Create Question_1.sql

---
+++ Day_9/Question_1.sql
@@ -0,0 +1,18 @@
+/*
+MYSQL : Scheduling Errors
+Write a query which displays the year in ascending order ,find for each semester how many professors are involved in delivering the session for the courses 1 to 10.
+
+The output should contain the columns : year|semester|no_of_professor
+no_of_professor: Total number of professor
+
+*/
+SELECT 
+    year,
+    semester,
+    COUNT(DISTINCT professor_id) AS no_of_professor
+FROM 
+    schedule
+GROUP BY 
+    year, semester
+ORDER BY 
+    year ASC, semester ASC;
